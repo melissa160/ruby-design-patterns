ff2113093b13df2112cccb5a37fe1f8fa5c4da52
melissa <melissa160@gmail.com>
2019-02-17 20:48:17 -0500

    template-method-pattern

---
+++ template-method-pattern/main.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require './html_report'
+require './plain_text_report'
+
+report = HTMLReport.new
+report.output_report
+
+report = PlainTextReport.new
+report.output_report
---
+++ template-method-pattern/plain_text_report.rb
@@ -0,0 +1,11 @@
+require './report'
+class PlainTextReport < Report
+    def output_head
+        puts("**** #{@title} ****")
+        puts
+    end
+
+    def output_line(line)
+        puts(line)
+    end
+end 
